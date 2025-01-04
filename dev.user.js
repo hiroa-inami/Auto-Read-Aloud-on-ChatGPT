62f05ae8b625331773ce96519e88a7a197398fe6
hiroa-inami <hiroa.inami@gmail.com>
2025-01-04 13:33:51 +0900

    Create dev.user.js

---
+++ dev.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Dummy script to require the script
+// @namespace    https://github.com/iha-hiroa/
+// @copyright    MIT
+// @version      0.1
+// @description  Dummy script to @require the script
+// @author       hiroa
+// @match        https://chatgpt.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
+// @grant        none
+// @require      https://github.com/hiroa-inami/Auto-Read-Aloud-on-ChatGPT/raw/refs/heads/0.2/main.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Dummy script to @require the script.
+    // Now I can write script on a real editor.
+})();
