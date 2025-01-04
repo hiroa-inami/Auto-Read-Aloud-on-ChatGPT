0a225570254e020c47e5bc86e6d2c96d7ecd59fb
hiroa-inami <hiroa.inami@gmail.com>
2025-01-04 13:25:50 +0900

    Update main.user.js
    rewrited whole script

--- main.user.js
+++ main.user.js
@@ -2,7 +2,7 @@
 // @name         Auto Read Aloud on ChatGPT
 // @namespace    https://github.com/iha-hiroa/
 // @copyright    MIT
-// @version      0.1.1
+// @version      0.2
 // @description  Automatically clicks "Read aloud" buttons on chatgpt.com
 // @author       hiroa
 // @match        https://chatgpt.com/*
@@ -12,20 +12,73 @@
 
 (function() {
     'use strict';
-    console.log(`Auto Read Aloud on ChatGPT: loaded`);
+    const playedTurns = []
+    let talkingTurn = null
+    const PARSE_INTERVAL = 400;
 
-    const clickedButtons = new WeakSet();
+    function getGptTurns(){
+        const h6Elements = document.querySelectorAll('h6.sr-only');
+        const saidElements = Array.from(h6Elements).filter(heading => {
+            return heading.textContent.trim() === "ChatGPT said:";
+        });
+        const gptTurns = saidElements.map(element => {
+            return element.parentElement;
+        });
+        return gptTurns
+    }
 
-    function checkAndClickButtons() {
-        const buttons = document.querySelectorAll('button[aria-label="Read aloud"]');
-        if (buttons.length > 0){
-            let button = buttons[buttons.length - 1]
-            if (clickedButtons.has(button)) return;
-            button.click();
-            clickedButtons.add(button);
+    function getOldTurns(){
+        const oldResponses = []
+        const gptTurns = getGptTurns();
+        for (const [i, turn] of gptTurns.entries()) {
+            oldResponses.push(turn);
+        }
+        return oldResponses
+    }
+
+    function getNewTurn(){
+        const gptTurns = getGptTurns();
+        for (const [i, turn] of gptTurns.entries()) {
+            if (!playedTurns.includes(turn)){
+                return turn
+            }
         }
     }
 
-    const interval = 1000; // Check every 1 second
-    setInterval(checkAndClickButtons, interval);
+    function getTalkButton(turn){
+        return turn.querySelector('button[aria-label="Read aloud"]')
+    }
+
+    function letGptTalk(turn){
+        if(!turn){return}
+        if(talkingTurn){return}
+        const button = getTalkButton(turn)
+        if(!button){return}
+
+        button.click()
+        talkingTurn = turn
+        playedTurns.push(turn)
+    }
+
+    function checkTalkIsFinished(turn){
+        if (turn === null){return}
+        const button = getTalkButton(turn)
+        if(button){
+            talkingTurn = null
+        }
+    }
+
+    function parseMain(){
+        if (talkingTurn){ checkTalkIsFinished(talkingTurn) }
+        const newTurn = getNewTurn()
+        if (newTurn) {letGptTalk(newTurn)}
+    }
+
+    function init(){
+        console.log("init: Auto Read Aloud on ChatGPT")
+        playedTurns.push(...getOldTurns())
+        setInterval(parseMain, PARSE_INTERVAL)
+    }
+
+    setTimeout(init, 3000);
 })();
